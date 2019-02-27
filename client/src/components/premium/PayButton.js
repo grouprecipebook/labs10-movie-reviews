cf5e120fe3457558f75281da78d43c4c4a24489f
ari7946 <mercado7946@gmail.com>
2019-02-26 16:44:42 -0800

    added comments to the Pay Now Component

--- client/src/components/premium/PayButton.js
+++ client/src/components/premium/PayButton.js
@@ -6,13 +6,21 @@ const PayButton = props => {
   const publishableKey = "pk_test_GAKbu7bXAJ5UZjNKNbWEe0XF";
 
   function onToken(token) {
+  // Ontoken sends information about the card to Stripe.
+  // Then it receives a token object that contains details about the transaction.
+  //(card type, last 4 digits, email, ect)s, email, ect)
     console.log("token", token)
     const body = {
+    // the body object contains our token from Stripe, 
+    // the total amount of cents, and the props that specify 
+    // the type of subscription, yearly or monthly
       amount: props.totalCents,
       token: token,
       plan: props.header,
   };
 
+  // we send the body object to our backend with an axios request.
+  // Our backend will then create a new customer and a new subscription
   axios
     .post("http://localhost:5000/api/payment", body)
     .then(response => {
@@ -43,8 +51,8 @@ const PayButton = props => {
         description={"$0.99"}
         panelLabel="Submit Payment" //Submit button in modal
         amount={99} //Amount in cents 
-        token={res => onToken(res)}
-        stripeKey={publishableKey}
+        token={res => onToken(res)} // onToken is invoked here
+        stripeKey={publishableKey} 
         // image="" //Pop-in header image
         billingAddress={false}
       />
