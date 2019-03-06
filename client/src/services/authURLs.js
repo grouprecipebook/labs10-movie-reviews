fa35f6af7e42bee61caca8d88eb79f4f39a1a0e0
Tom Tarpey <tomtarpeydev@gmail.com>
2019-03-06 14:55:43 +0000

    Update authURLs.js
    messing with some debugging

--- client/src/services/authURLs.js
+++ client/src/services/authURLs.js
@@ -1,14 +1,5 @@
-export const twitterLogin =
-  process.env.NODE_ENV === 'production'
-    ? 'https://labs10-movie-reviews.herokuapp.com/auth/twitter'
-    : 'http://localhost:5000/auth/twitter';
+export const twitterLogin = 'https://labs10-movie-reviews.herokuapp.com/auth/twitter';
 
-export const logout =
-  process.env.NODE_ENV === 'production'
-    ? 'https://labs10-movie-reviews.herokuapp.com/auth/logout'
-    : 'http://localhost:5000/auth/logout';
+export const logout = 'https://labs10-movie-reviews.herokuapp.com/auth/logout';
 
-export const googleLogin =
-  process.env.NODE_ENV === 'production'
-    ? 'https://labs10-movie-reviews.herokuapp.com/auth/google'
-    : 'http://localhost:5000/auth/google';
+export const googleLogin = 'https://labs10-movie-reviews.herokuapp.com/auth/google';
